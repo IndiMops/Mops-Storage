3123185c871689c12e14a0ae9cf8c8a9c9056967
INDMops <68908322+INDMops@users.noreply.github.com>
2022-05-14 16:51:57 +0300

    Redesine

--- jquery.js
+++ jquery.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#hider").click(function(){
-      $("#alert").slideToggle("slow");
-      $(this).toggleClass("active");
+    $("#alert").slideToggle("slow");
+    $(this).toggleClass("active");
+    $(".page-container-home").css("min-height", "90%");
   });
 });
