be98044963aa8741e3cade2e4905cada95fa56bd
INDMops <68908322+INDMops@users.noreply.github.com>
2022-06-29 02:24:55 +0300

    v0.20

--- jquery.js
+++ jquery.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     $(this).toggleClass("active");
     $(".page-container-home").css("min-height", "90%");
   });
+
+  $(".deteils-titlet").click(function(){
+    $(".deteils-desc").slideToggle("slow");
+    $(this).toggleClass("active");
+  });
+
+  //$(".one").hover(function() {
+    //if ($(this).attr("src") == "assets/gallery-cover/red-dead-redemption-2.jpg") {
+      //$(this).attr("src", "assets/gallery-cover/2.jpg");
+    //} else {
+      //$(this).attr('src', "assets/gallery-cover/red-dead-redemption-2.jpg");
+    //}
+  //});
 });
